d6fe75b5fdf1f1ddc1880ab8fb2532f8c3d8475b
Kevin Youkhana <kyoukhana@gmail.com>
2020-04-03 10:33:26 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import $buzz from 'musquito';
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
